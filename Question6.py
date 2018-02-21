57bd13d2103881317fcdb630dcab626da9115fd1
arpita <asakhuj@ncsu.edu>
2018-02-21 18:58:14 -0500

    Added the logger part without if condition

--- Question6.py
+++ Question6.py
@@ -4,6 +4,8 @@ import libvirt
 import time
 conn = libvirt.open('qemu:///system')
 
+threshhold = raw_input('Please enter the value of threshold in milliseconds')
+
 response = raw_input('Which parameter you want to choose to see the VMs, CPU (press c) or Memory (press m) ')
 
 def getMemoryBased(item):
@@ -31,7 +33,16 @@ for id in conn.listDomainsID():
         cpuTime2 = cpuStats2[0]["cpu_time"]/1000000
         #print(cpuStats2)
 	#cpuStats = cpuStats["vcpu_time"]
-        cpuPercent =  (cpuTime2- cpuTime1)*100/5
+        cpuPercent =  (cpuTime2- cpuTime1)/3
+        print(cpuPercent)
+        print("Threshold is ")
+        print(threshhold)
+#        if cpuPercent > threshhold :
+        print('inside if')
+        localTime = time.asctime( time.localtime(time.time()) )
+        file = open('/home/asakhuj/hw2/log.txt','a+')
+        file.write("VM : "+str(domName) + " "+str(localTime) +" CPU Usage : in milliseconds "+str(cpuPercent));
+        file.write("\n") 
         #print(cpuPercent)
 	newArr = []
 	newArr.append(domName)
@@ -51,3 +62,4 @@ else :
      arr_final = sorted(arr_vm,key=getMemoryBased)
 print("Sorted array ")
 print(arr_final)	
+#file.close()
