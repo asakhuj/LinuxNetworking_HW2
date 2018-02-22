0f29f447f0bda6ce10e6f491f08ec85003ad68c9
asakhuj@ncsu.edu <asakhuj@ncsu.edu>
2018-02-22 02:09:05 -0500

    Formatting

--- Question6.py
+++ Question6.py
@@ -24,36 +24,24 @@ for id in conn.listDomainsID():
         availableMemory = mem["available"]
         usedMemory =  availableMemory - unusedMemory
 	cpuStats1 = dom.getCPUStats(True)
-        #print(cpuStats1)
         #In milliseconds
 	cpuTime1 = cpuStats1[0]["cpu_time"]/1000000
-	 #print(cpuTime1)
-        time.sleep(3);
+        time.sleep(3);  #Sleeping for 3 seconds for creating samples for CPU Usage
         cpuStats2 = dom.getCPUStats(True)
         cpuTime2 = cpuStats2[0]["cpu_time"]/1000000
-        #print(cpuStats2)
-	#cpuStats = cpuStats["vcpu_time"]
         cpuPercent =  (cpuTime2- cpuTime1)/3
-        #print("CPU percent is ",cpuPercent,"type : ",type(cpuPercent))
-       # print("Threshold is ",threshold,"type is",type(threshold))
-       # print(int(cpuPercent) > int(threshold))
+        #Add loggers if CPU utilization is greater than threshold
         if (int(cpuPercent) > int(threshold)) :
-            #print('inside if')
             localTime = time.asctime( time.localtime(time.time()) )
             file = open('log.txt','a+')
             file.write("VM : "+str(domName) + " "+str(localTime) +" CPU Usage : in milliseconds "+str(cpuPercent));
             file.write("\n") 
-        #print(cpuPercent)
 	newArr = []
 	newArr.append(domName)
 	newArr.append(usedMemory)
         newArr.append(cpuPercent)
-	#domInfo = dom.info()
-	#newArr.append(domInfo[4])
-	#newArr.append(vcpuStats)
-	#print(newArr)
 	arr_vm.append(newArr)	
-#print(arr_vm)
+
 arr_final = []
 if response == 'c' :
       arr_final = sorted(arr_vm,key=getCPUBased)
